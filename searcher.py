8c1f0d855c15cdee956c226522f1fb8362a590a2
Stephanie A. Higa <sahiga@gmail.com>
2015-03-14 23:25:17 -0700

    Returning photo descriptions

--- searcher.py
+++ searcher.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-import flickrapi, nltk, re, urllib2, os
-import xml.etree.ElementTree as ET
-from bs4 import BeautifulSoup
+import flickrapi, unirest, time, os
 
 api_key = u'bd357bbf22b783cd194d9585e495e0a4'
 api_secret = u'8be92d6e6304428a'
@@ -36,10 +34,46 @@ def search_flickr(query):
       '.staticflickr.com/' + attributes['server'] + '/' +
       attributes['id'] + '_' + attributes['secret'] + '_m.jpg')
 
-  return photo_urls
+  for url in photo_urls:
+    read_image(url)
 
-  # input urls into cloudsight api
+def read_image(photo_url):
+  post_res = unirest.post("https://camfind.p.mashape.com/image_requests",
+    headers = {
+      "X-Mashape-Key": "6qRcLmRhtpmshHUnKZhr35Tkpf4Ep18I4HbjsndLZjL7cUMrwt",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/json"
+    },
+    params = {
+      "focus[x]": "480",
+      "focus[y]": "640",
+      "image_request[altitude]": "27.912109375",
+      "image_request[language]": "en",
+      "image_request[latitude]": "35.8714220766008",
+      "image_request[locale]": "en_US",
+      "image_request[longitude]": "14.3583203002251",
+      "image_request[remote_image_url]": photo_url
+    }
+  )
+  
+  return get_description(post_res.body['token'])
 
-  # wait
+def check_res(token):
+  time.sleep(20)
+  
+  response = unirest.get("https://camfind.p.mashape.com/image_responses/" + token,
+    headers = {
+      "X-Mashape-Key": "6qRcLmRhtpmshHUnKZhr35Tkpf4Ep18I4HbjsndLZjL7cUMrwt",
+      "Accept": "application/json"
+    }
+  )
+ 
+  return response
 
-  # copy resulting description into links
+def get_description(token):
+  description = check_res(token)
+
+  if description.body.has_key('name') == False:
+    description = check_res(token)
+
+  return description.body['name']
